c0efc7ead7015d3733534c8166a997e85c26d9d8
suba14 <mot.schubert@gmail.com>
2021-05-18 22:21:02 +0200

    heroku

--- Tom_4328112/__init__.py
+++ Tom_4328112/__init__.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 if ENV =='dev':
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///LTApps.db'
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://mksxavarujfwmi:25e05328a54d17914a2cd1a231d29ac0e942a3dd7411213701178fa7441255ef@ec2-54-163-254-204.compute-1.amazonaws.com:5432/d562uvts2a0r57'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://csxkuyiwuzeryk:dfc99d05f47b0cf76259f3abfed1e06dfc49c1cbe03ab0ddecfa572ddd46580a@ec2-63-34-97-163.eu-west-1.compute.amazonaws.com:5432/dbl4um2jdt8q85'
 # Konfiguration eines SecretKeys, mit welchem Daten beim Suer verschlüsslet werden, um Cross Site Script Angriffe zu verhindern
 app.config['SECRET_KEY'] = 'ed01997c4a2b400537bf2260a3593d04'
 # Konfiguration einer SQLITE Datenbankanbindung mit relativem Pfad von dieser Appliaktion aus
