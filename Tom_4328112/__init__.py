133de66a2aced3d94608e8a0fb2a7d4febf92aca
suba14 <mot.schubert@gmail.com>
2021-05-18 23:12:37 +0200

    deploy

--- Tom_4328112/__init__.py
+++ Tom_4328112/__init__.py
@@ -8,11 +8,15 @@ from flask_socketio import SocketIO
 # deklariert die gesamte Appliaktion als Flask Applikation mit dem Namen __main__, falls sie aus diesem Skript gestartet wird
 app = Flask(__name__)
 
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://rklqhnwumcasxo:c9a8a9c132dc639ed63c5bb82fd0124f687b7a347ec6ea79b192be309e5bc84d@ec2-63-34-97-163.eu-west-1.compute.amazonaws.com:5432/ddhjl9a0su7cvc'
+ENV='production'
+if ENV == 'dev':
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///LTApps.db'
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://rklqhnwumcasxo:c9a8a9c132dc639ed63c5bb82fd0124f687b7a347ec6ea79b192be309e5bc84d@ec2-63-34-97-163.eu-west-1.compute.amazonaws.com:5432/ddhjl9a0su7cvc'
 # Konfiguration eines SecretKeys, mit welchem Daten beim Suer verschlüsslet werden, um Cross Site Script Angriffe zu verhindern
 app.config['SECRET_KEY'] = 'ed01997c4a2b400537bf2260a3593d04'
 # Konfiguration einer SQLITE Datenbankanbindung mit relativem Pfad von dieser Appliaktion aus
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
